33c3371af2c7afaf62f53b0fab1f8f4bbab42f98
Ainyaku <87048351+Ainyaku@users.noreply.github.com>
2022-04-17 03:35:55 -0400

    Added issue link in uninstall message

--- !Uninstall.bat
+++ !Uninstall.bat
@@ -2,6 +2,6 @@
 del "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\SendTo\USC Song Folder.lnk"
 del "%USERPROFILE%\AinyakuUSCSongInstallerFileLocation.txt"
 set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
-echo MsgBox "Ainyaku's USC Song Installer has been uninstalled. Please report any problems in the issues section of the Github page.",64,"Ainyaku's USC Song Installer" >> %SCRIPT%
+echo MsgBox "Ainyaku's USC Song Installer has been uninstalled. Please report any problems at https://r.ainyaku.com/USCprob",64,"Ainyaku's USC Song Installer" >> %SCRIPT%
 cscript /nologo %SCRIPT%
 del %SCRIPT%
