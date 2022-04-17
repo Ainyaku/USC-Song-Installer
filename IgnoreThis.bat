2c4844d4cda5d09b151df7379b1a24b9d63e5cc5
Ainyaku <87048351+Ainyaku@users.noreply.github.com>
2022-04-17 01:45:12 -0400

    Add files via upload

--- IgnoreThis.bat
+++ IgnoreThis.bat
@@ -1,7 +1,7 @@
 @echo off
 for /f "tokens=* delims=" %%x in ('type "%USERPROFILE%\AinyakuUSCSongInstallerFileLocation.txt"') do set files=%%x
 for /f "tokens=* delims=" %%x in ('type "%files%\!ChangeToSongFolder.txt"') do set location=%%x
-powershell -command "Expand-Archive -Force '%1' '%location%\%~n1'"
+powershell -command "Expand-Archive -Force '%1' '%location%\Ainyaku Installer\%~n1'"
 set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 echo MsgBox "Song installed.",64,"Ainyaku's USC Song Installer" >> %SCRIPT%
 cscript /nologo %SCRIPT%
